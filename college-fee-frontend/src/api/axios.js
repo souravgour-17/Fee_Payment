2159bab8c87b7a371b6fd9d532523a2b766a6f43
souravgour-17 <souravgour798@gmail.com>
2025-09-04 12:09:09 +0530

    Update axios.js baseURL to Render backend

--- college-fee-frontend/src/api/axios.js
+++ college-fee-frontend/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 // Create axios instance
 const api = axios.create({
-  baseURL: "https://souravian-university.onrender.com/api", //   http://localhost:5000/api
+  baseURL: "https://souravian-university.onrender.com", //   http://localhost:5000/api
   headers: {
     "Content-Type": "application/json",
   },
